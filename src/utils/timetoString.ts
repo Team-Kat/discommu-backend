514ec0e23f60ca0be59eaa7f874753c27d5f504c
kiki7000 <devkiki7000@gmail.com>
2021-04-04 19:39:07 +0900

    feat: timetoString

---
+++ src/utils/timetoString.ts
@@ -0,0 +1,12 @@
+const timetoString = stamp => 
+    new Date(stamp)
+        .toLocaleString(undefined, {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+            second: "numeric",
+            hour: "numeric",
+            minute: "numeric"
+        })
+
+export default timetoString;
