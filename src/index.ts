6473a0740fc3d19bda3cbcfae21c93924609061a
kiki7000 <devkiki7000@gmail.com>
2021-04-29 14:05:34 +0900

    feat: userCache

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,20 @@ import "reflect-metadata";
 import Express from "express";
 import { buildSchema } from "type-graphql";
 import { ApolloServer } from "apollo-server-express";
+import { verify } from "jsonwebtoken";
 
 import { dbConnect, dbDisconnect } from "./database";
 
 import config from "../config.json";
 
 import logger from "./utils/logger";
+import { userCache } from "./utils/cache";
 
 import Log from "./middlewares/Log";
 
 import DefaultResolver from "./resolvers/DefaultResolver";
+
+import TContext from "./types/context";
 
 process.on("exit", () => {
     logger.info("Exit");
@@ -31,7 +35,26 @@ process.on("exit", () => {
 
     const apollo = new ApolloServer({
         schema,
-        logger
+        logger,
+        context: async ({ req }): Promise<TContext> => {
+            if (!req.headers.authorization) return null;
+            if (!req.headers.authorization.startsWith("Bearer ")) return null;
+            const token = req.headers.authorization.slice("Bearer ".length);
+
+            const uCache = new userCache();
+            let user;
+
+            try { user = await uCache.getUser((verify(token, config.jwtSecret) as any).id) }
+            catch { user = null }
+
+            if (!user)
+                user = null;
+
+            return {
+                userCache: uCache,
+                user: user
+            }
+        }
     });
 
     const app: Express.Application = Express();
--- src/types/context.ts
+++ src/types/context.ts
@@ -1,6 +1,8 @@
 import { TUser } from "./user";
+import { userCache } from "../utils/cache"
 
 type TContext = {
     user: TUser
+    userCache: userCache
 }
 export default TContext
