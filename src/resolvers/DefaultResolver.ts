f15ff4d4545cc353678200ac19f5668fdd30f7a9
kiki7000 <devkiki7000@gmail.com>
2021-05-01 18:59:22 +0900

    feat: users limit

--- src/resolvers/DefaultResolver.ts
+++ src/resolvers/DefaultResolver.ts
@@ -2,6 +2,7 @@ import { Resolver, Query, Ctx, Arg, Info } from "type-graphql";
 import { GraphQLResolveInfo } from "graphql";
 
 import graphqlFields from "graphql-fields";
+import { ApolloError } from "apollo-server-errors";
 
 import TContext from "../types/context";
 import { GraphQLTUser } from "../types/graphql/User";
@@ -37,9 +38,20 @@ export default class DefaultResolver {
     }
 
     @Query(returns => [GraphQLTUser])
-    async users(@Ctx() ctx: TContext, @Info() info: GraphQLResolveInfo) {
-        const users = await UserModel.find({}, Object.keys(graphqlFields(info)).join(" "));
+    async users(
+        @Ctx() ctx: TContext,
+        @Info() info: GraphQLResolveInfo,
+        @Arg("limit", { nullable: true, description: "How many users to divide" }) limit?: number,
+        @Arg("limitIndex", { defaultValue: 1, description: "Index of divided users", nullable: true }) limitIndex?: number
+    ) {
+        if (limitIndex <= 0)
+            throw new ApolloError("limitIndex should be a natural number", "TYPE_ERROR")
+
         let res = [];
+        let users = await UserModel.find({}, Object.keys(graphqlFields(info)).join(" "), {
+            limit: limit || undefined,
+            skip: (limitIndex - 1) * limit
+        }).exec();
 
         for (const dbUser of users) {
             const user = await dbUser.getUser(ctx.userCache);
