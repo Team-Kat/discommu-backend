bb80801659c272656fa9bbc2e14afd2525da8a15
kiki7000 <devkiki7000@gmail.com>
2021-05-05 17:42:44 +0900

    refactor: filters

--- src/middlewares/Authorization.ts
+++ src/middlewares/Authorization.ts
@@ -1,8 +1,9 @@
 import { AuthChecker } from "type-graphql";
+import { ApolloError } from "apollo-server-errors";
 
 import TContext from "../types/context";
 
-const DiscommuAuthChecker: AuthChecker<TContext> = ({ context }, roles: string[]) => {
+const DiscommuAuthChecker: AuthChecker<TContext> = ({ context, args }, roles: string[]) => {
     if (!context.user)
         return false;
 
@@ -14,7 +15,15 @@ const DiscommuAuthChecker: AuthChecker<TContext> = ({ context }, roles: string[]
         switch (role) {
             case "ADMIN":
                 if (context.user.permissions.includes("admin"))
+                    return true;
+                else
+                    res = false;
+                break;
+            case "USER_EDIT":
+                if (args.id && ((args.id === context.user.discordID) || context.user.permissions.includes("admin")))
                     res = res;
+                else
+                    res = false;
                 break;
         }
     }
--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -6,9 +6,12 @@ import TUser from "../types/user";
 import TContext from "../types/context";
 import GraphQLTUser from "../types/graphql/User";
 
+import getElements from "../utils/getElements";
+
 import EditUser from "../inputs/EditUser";
 
-import badges from "../data/json/badges.json";
+import badgesInfo from "../data/json/badges.json";
+import permissionsInfo from "../data/json/permissions.json";
 
 @Resolver(GraphQLTUser)
 export default class {
@@ -47,12 +50,12 @@ export default class {
         let res = [];
 
         for (const badge of root.badges) {
-            if (!badges[badge])
+            if (!badgesInfo[badge])
                 continue;
 
             res.push({
                 "name": badge,
-                ...badges[badge]
+                ...badgesInfo[badge]
             })
         }
 
@@ -87,20 +90,84 @@ export default class {
         return res;
     }
 
-    @Authorized()
+
+    @Authorized(["USER_EDIT"])
+    @Mutation(returns => GraphQLTUser)
+    async editUserDescription(@Ctx() ctx: TContext, @Arg("id") userID: string, @Arg("description") description: string) {
+        if (description.length >= 100)
+            throw new ApolloError("description must be shorter than or equal to 100 characters.", "FIELD_LENGTH_OVER");
+
+        await UserModel.updateOne({ discordID: userID }, { $set: { description: description } });
+        return await ctx.userCache.getUser(userID);
+    }
+
+
+    @Authorized(["USER_EDIT"])
+    @Mutation(returns => GraphQLTUser)
+    async editUserFollowing(@Ctx() ctx: TContext, @Arg("id") userID: string, @Arg("following", type => [String]) following: string[]) {
+        let newFollowing = [];
+        for (const fID of getElements(ctx.user.following, following)) {
+            if (fID === userID)
+                continue;
+            if (await UserModel.exists({ discordID: fID }))
+                newFollowing.push(fID);
+        }
+
+        await UserModel.updateOne({ discordID: userID }, { $set: { following: newFollowing } });
+        return await ctx.userCache.getUser(userID);
+    }
+
+
+    @Authorized(["ADMIN"])
+    @Mutation(returns => GraphQLTUser)
+    async editUserBadges(@Ctx() ctx: TContext, @Arg("id") userID: string, @Arg("badges", type => [String]) badges: string[]) {
+        let newBadges = [];
+        for (const badge of getElements(ctx.user.badges, badges)) {
+            if (badgesInfo[badge])
+                newBadges.push(badge);
+        }
+
+        await UserModel.updateOne({ discordID: userID }, { $set: { badges: newBadges } });
+        return await ctx.userCache.getUser(userID);
+    }
+
+
+    @Authorized(["ADMIN"])
+    @Mutation(returns => GraphQLTUser)
+    async editUserPermissions(@Ctx() ctx: TContext, @Arg("id") userID: string, @Arg("permissions", type => [String]) permissions: string[]) {
+        let newPermissions = [];
+        for (const permission of getElements(ctx.user.permissions, permissions)) {
+            if (permissionsInfo.includes(permission))
+                newPermissions.push(permission);
+        }
+
+        await UserModel.updateOne({ discordID: userID }, { $set: { permissions: newPermissions } });
+        return await ctx.userCache.getUser(userID);
+    }
+
+
+    @Authorized(["USER_EDIT"])
     @Mutation(returns => GraphQLTUser, { nullable: true })
     async editUser(@Ctx() ctx: TContext, @Arg("id") userID: string, @Arg("data") data: EditUser) {
-        if ((userID !== ctx.user.discordID) && !ctx.user.permissions.includes("admin"))
-            throw new ApolloError("You can only edit your info", "NO_PERMISSION")
+        if ((data.permissions || data.badges) && !ctx.user.permissions.includes("admin"))
+            throw new ApolloError("To edit permissions or badges, you need the administer permission", "NO_PERMISSION");
 
-        if ((data.permissions || data.badges) && !(ctx.user.permissions.includes("MODIFY_POINTS") || ctx.user.permissions.includes("admin")))
-            throw new ApolloError("To edit permissions, you need the administer permission", "NO_PERMISSION");
+        const user = await UserModel.findOne({ discordID: userID });
+        if (!user)
+            return null;
 
-        return await ctx.userCache.getUser(
-            (await UserModel.findOneAndUpdate(
-                { discordID: userID },
-                { ...data },
-                { new: true })).discordID
-        );
+        if (data.description)
+            await this.editUserDescription(ctx, userID, data.description);
+
+        if (data.following)
+            await this.editUserFollowing(ctx, userID, data.following);
+
+        if (data.badges)
+            await this.editUserBadges(ctx, userID, data.badges);
+
+        if (data.permissions)
+            await this.editUserPermissions(ctx, userID, data.permissions);
+
+        return await ctx.userCache.getUser(userID);
     }
 }
