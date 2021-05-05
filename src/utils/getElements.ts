06cec637fac443184fe614e4f5fee8d026748d37
kiki7000 <devkiki7000@gmail.com>
2021-05-05 13:30:44 +0900

    feat: getElements

---
+++ src/utils/getElements.ts
@@ -0,0 +1,22 @@
+const getElements = (defaultElements: string[], elements?: string[]) => {
+    let res = [];
+    res = defaultElements;
+    if (elements.length === 1) {
+        switch (elements[0][0]) {
+            case "+":
+                res.push(elements[0].slice(1))
+                break;
+            case "-":
+                res = res.filter(b => b !== elements[0].slice(1));
+                break
+            default:
+                res = elements;
+        }
+    }
+    else
+        res = elements;
+
+    return res;
+}
+
+export default getElements;
