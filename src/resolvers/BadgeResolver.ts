eec01b6c9ed036f0a0d73d3f6b912953a8d48cca
kiki7000 <devkiki7000@gmail.com>
2021-05-08 22:09:37 +0900

    feat: TBadge

--- src/resolvers/BadgeResolver.ts
+++ src/resolvers/BadgeResolver.ts
@@ -1,23 +1,25 @@
 import { FieldResolver, Resolver, Root, Ctx } from "type-graphql";
-import TContext from "../types/context";
 import GraphQLTBadge from "../types/graphql/Badge";
+
+import TContext from "../types/context";
+import TBadge from "../types/badge";
 
 import config from "../../config.json";
 
 @Resolver(GraphQLTBadge)
 export default class {
     @FieldResolver()
-    async name(@Root() root: GraphQLTBadge) {
+    async name(@Root() root: TBadge) {
         return root.name;
     }
 
     @FieldResolver()
-    async icon(@Root() root: GraphQLTBadge, @Ctx() ctx: TContext) {
+    async description(@Root() root: TBadge, @Ctx() ctx: TContext) {
         return ctx.url + "/" + config.staticDir + "/" + root.icon;
     }
 
     @FieldResolver()
-    async permissions(@Root() root: GraphQLTBadge) {
+    async permissions(@Root() root: TBadge) {
         return root.permissions;
     }
 }
