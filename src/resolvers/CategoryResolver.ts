60e4352b693287fa0dcfb0ec86766b63c3fd0f8f
kiki7000 <devkiki7000@gmail.com>
2021-05-09 00:31:33 +0900

    feat: createCategory

--- src/resolvers/CategoryResolver.ts
+++ src/resolvers/CategoryResolver.ts
@@ -1,8 +1,12 @@
-import { FieldResolver, Resolver, Root, Ctx } from "type-graphql";
+import { FieldResolver, Resolver, Root, Ctx, Mutation, Authorized, Arg } from "type-graphql";
+import { ApolloError } from "apollo-server-errors";
 import GraphQLTCategory from "../types/graphql/Category";
 
 import TContext from "../types/context";
 import { TCategory } from "../types/category";
+
+import CreateCategory from "../inputs/CreateCategory";
+import { CategoryModel } from "../database";
 
 @Resolver(GraphQLTCategory)
 export default class {
@@ -24,5 +28,14 @@ export default class {
     @FieldResolver()
     async author(@Root() root: TCategory, @Ctx() ctx: TContext) {
         return await ctx.userCache.getUser(root.authorID);
+    }
+
+    @Authorized(["MODIFY_CATEGORIES"])
+    @Mutation(returns => GraphQLTCategory)
+    async createCategory(@Ctx() ctx: TContext, @Arg("data") data: CreateCategory) {
+        if (await CategoryModel.exists({ name: data.name }))
+            throw new ApolloError(`Category with name ${data.name} exists`, "CATEGORY_EXISTS");
+
+        return await CategoryModel.create({ name: data.name, description: data.description, type: 1, authorID: ctx.user.discordID });
     }
 }
