adabfad78702e8cf04453c60e57aefc5a3e123ba
kiki7000 <devkiki7000@gmail.com>
2021-05-11 12:17:03 +0900

    feat: authorization SELF_POST

--- src/middlewares/Authorization.ts
+++ src/middlewares/Authorization.ts
@@ -1,6 +1,6 @@
 import { AuthChecker } from "type-graphql";
 
-import { CategoryModel } from "../database";
+import { CategoryModel, PostModel } from "../database";
 
 import TContext from "../types/context";
 
@@ -21,7 +21,7 @@ const DiscommuAuthChecker: AuthChecker<TContext> = async ({ context, args }, rol
                 res = false;
                 break;
             case "USER_EDIT":
-                if (args.id && ((args.id === context.user.discordID) || context.user.permissions.includes("admin")))
+                if (args.id && (args.id === context.user.discordID))
                     res = res;
                 else
                     res = false;
@@ -33,9 +33,7 @@ const DiscommuAuthChecker: AuthChecker<TContext> = async ({ context, args }, rol
                     res = false;
                 break;
             case "SELF_CATEGORY":
-                if (context.user.permissions.includes("admin"))
-                    res = res;
-                else if (!context.user.permissions.includes("MODIFY_CATEGORIES"))
+                if (!context.user.permissions.includes("MODIFY_CATEGORIES"))
                     res = false;
                 else {
                     const category = await CategoryModel.findOne({ name: args.name });
@@ -45,6 +43,13 @@ const DiscommuAuthChecker: AuthChecker<TContext> = async ({ context, args }, rol
                         res = true;
                 }
 
+                break;
+            case "SELF_POST":
+                const post = await PostModel.findById(args.id);
+                if (post?.authorID !== context.user.discordID)
+                    res = false;
+                else
+                    res = true;
                 break;
         }
     }
